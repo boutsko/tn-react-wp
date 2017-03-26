eadf86725d4794dfa3475e4ca2a743c8abcb84f4
boutsko <boutsko@gmail.com>
2017-03-26 14:30:08 +0300

    add handleLike to BlockPage

--- src/components/Like.js
+++ src/components/Like.js
@@ -10,21 +10,26 @@ class Like extends Component {
     likes: 0
   }
   
-  state = {
-    likes: this.props.likes
-  }
+  /* state = {
+   *   likes: this.props.likes
+   * }
 
-  handleClick = (ev) => {
-    this.setState({likes: this.state.likes + 1 })
-  }
+   * handleClick = (ev) => {
+   *   this.setState({likes: this.state.likes + 1 })
+   * }*/
   
   render() {
+	const { likes, handleLike } = this.props
     return (
       <div>
-        <a href="#" onClick={this.handleClick}>{this.state.likes}</a>
+        <a href="#" onClick={handleLike(1)}>{likes}</a>
       </div>
     )
   }
 }
 
 export default Like
+
+
+/* <a href="#" onClick={this.handleClick}>{this.state.likes}</a>
+ * handleLike={this.props.handleLike}*/
