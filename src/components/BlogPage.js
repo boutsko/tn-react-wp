6be410eaa756854fe9531bb65ea83a99ae4ef99f
boutsko <boutsko@gmail.com>
2017-03-28 11:18:39 +0300

    fix state immutable

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
+import { assign, map } from 'lodash'
 import BlogList from './BlogList'
 import PieChart from './PieChart'
+
 
 class BlogPage extends Component {
   constructor(props) {
@@ -8,26 +10,27 @@ class BlogPage extends Component {
   }
 
   state = {
-	posts: this.props.posts
+    posts: this.props.posts
   }
 
   handleLike = postId => ev => {
-	const newPosts =  this.state.posts.map(post => {
-	  post.id === postId && post.likes++
-	  return post;
+    const postsClone = assign({}, this.state.posts)
+    const newPosts = map(postsClone, (post) => {
+      post.id === postId && ++post.likes
+      return post;
     })
     this.setState({ posts: newPosts });
   }
 
   render() {
     const posts = this.props
-	const columns = this.props.posts.map(post => [post.line, post.likes])
-	
+    const columns = this.props.posts.map(post => [post.line, post.likes])
+    
     return (
-	  <div>
-		<BlogList {...posts} handleLike={this.handleLike}/>
-		<PieChart columns={columns}/>
-	  </div>
+      <div>
+        <BlogList {...posts} handleLike={this.handleLike}/>
+        <PieChart columns={columns}/>
+      </div>
     );
   }
 }
