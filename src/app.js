1cde70d2427d9c4f5a512e0362d17847327a27e5
boutsko <boutsko@gmail.com>
2017-04-02 10:03:32 +0300

    add semantic-ui

--- src/app.js
+++ src/app.js
@@ -2,5 +2,6 @@ import React from 'react'
 import {render} from 'react-dom'
 import BlogPage from './components/BlogPage'
 import {posts} from './fixtures'
+import '../semantic/dist/semantic.min.css'
 
 render(<BlogPage posts={posts}/>, document.getElementById('container'))
